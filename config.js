dbcabe7b987cd2958d4544021631a8b543900df5
Mike Vasios <mvasios1@gmail.com>
2018-04-06 21:21:55 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleAPI: 'AIzaSyB4xGNkuXIf0eeYQPJ0UuF-eGg_s-TJIQw',
-  host: 'seeker.c7l7qn51j1yo.us-east-2.rds.amazonaws.com',
-  user: 'HouseLannister',
-  password: 'hearmeroar',
-  port: '3306',
-  database: 'kindly'
-}
