a803936ad0f8b20386ab3c0eca157c177c05a089
Admin <admin@Admins-MacBook-Air.local>
2018-04-07 14:02:05 -0400

    final heroku

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -167,7 +167,7 @@ class App extends React.Component {
 
         <MapComponent
           isMarkerShown
-          googleMapURL=`https://maps.googleapis.com/maps/api/js?key=${process.env.secretKey}&v=3.exp&libraries=geometry,drawing,places`
+          googleMapURL=`https://maps.googleapis.com/maps/api/js?key=AIzaSyAnBJSrnGiKY_OHA1j3fTgyj2Y6rpvMgjk&v=3.exp&libraries=geometry,drawing,places`
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }}
